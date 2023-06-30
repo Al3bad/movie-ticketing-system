1cefaff9066746e5992365f525c0b8992c8969d0
Abdullah Alabbad <abdullah@alabbad.dev>
2023-06-30 17:20:50 +1000

    define controller functions for booking routes

--- backend/api/controller.booking.ts
+++ backend/api/controller.booking.ts
@@ -14,3 +14,29 @@ export const help = (req: Request, res: Response) => {
     ],
   });
 };
+
+export const createBooking = (req: Request, res: Response) => {
+  // validate data received from frontend
+
+  // call function to insert data in db
+
+  // if there is a problem -> res with an error
+
+  // otherwise -> res with OK status
+  res.end("IN PROGRESS");
+};
+
+export const getAllBookings = (req: Request, res: Response) => {
+  // TODO:
+  res.end("TODO");
+};
+
+export const getBookingById = (req: Request, res: Response) => {
+  // TODO:
+  res.end("TODO");
+};
+
+export const exportRecords = (req: Request, res: Response) => {
+  // TODO:
+  res.end("TODO");
+};
