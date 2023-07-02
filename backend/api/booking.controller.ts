6b51012885e40f8eed41b7d097af490a9029f5b8
Abdullah Alabbad <abdullah@alabbad.dev>
2023-07-03 01:28:27 +1000

    set status code and payload based on the error type in (POST /api/booking/) endpoint

--- backend/api/booking.controller.ts
+++ backend/api/booking.controller.ts
@@ -3,8 +3,11 @@ import { NewBookingSchema } from "@/common/validations";
 import { insertBooking } from "backend/dbQueries";
 import { Request, Response } from "express";
 
-const { HTTP_STATUS_OK, HTTP_STATUS_BAD_REQUEST, HTTP_STATUS_CREATED } =
-  constants;
+const {
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_CREATED,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+} = constants;
 
 export const help = (req: Request, res: Response) => {
   res.json({
@@ -28,7 +31,17 @@ export const createBooking = (req: Request, res: Response) => {
   if (result.success) {
     // call function to insert data in db
     const booking = insertBooking(result.data);
-    return res.status(HTTP_STATUS_CREATED).json(booking);
+    if (booking.error) {
+      return res
+        .status(
+          booking.error.type === "DB"
+            ? HTTP_STATUS_BAD_REQUEST
+            : HTTP_STATUS_INTERNAL_SERVER_ERROR
+        )
+        .json({ error: { msg: booking.error.msg } });
+    } else {
+      return res.status(HTTP_STATUS_CREATED).json(booking);
+    }
   } else {
     // if there is a problem -> res with an error
     return res.status(HTTP_STATUS_BAD_REQUEST).json({
