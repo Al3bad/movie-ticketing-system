6248ae828123711d73ae3e4646c089bdb4bf76ef
Abdullah Alabbad <abdullah@alabbad.dev>
2023-07-06 01:52:35 +1000

    add cypress testing framework / test movie routes

---
+++ backend/api/movie.test.ts
@@ -0,0 +1,62 @@
+describe("/api/movie", () => {
+  const apiUrl = Cypress.env("apiUrl");
+
+  before(() => {
+    cy.task("initDB");
+  });
+
+  it("GET / - Returns array of 4 elements", () => {
+    cy.request({
+      url: apiUrl + "/movie",
+      method: "GET",
+    })
+      .its("body")
+      .should("have.length", 4);
+  });
+
+  it("GET / - Returns all movies", () => {
+    cy.request({
+      url: apiUrl + "/movie",
+      method: "GET",
+    })
+      .its("body")
+      .should("deep.equal", [
+        {
+          title: "Avatar",
+          seatAvailable: 44,
+        },
+        {
+          title: "Frozen",
+          seatAvailable: 1,
+        },
+        {
+          title: "Spiderman",
+          seatAvailable: 38,
+        },
+        {
+          title: "Test",
+          seatAvailable: 50,
+        },
+      ]);
+  });
+
+  it("GET /help - Returns help object (routes)", () => {
+    cy.request({
+      url: apiUrl + "/movie/help",
+      method: "GET",
+    })
+      .its("body")
+      .should("have.property", "routes");
+  });
+
+  it("GET /notFound - Returns Not Found with help body", () => {
+    cy.request({
+      url: apiUrl + "/movie/notFound",
+      method: "GET",
+      failOnStatusCode: false,
+    }).should((res) => {
+      expect(res.status).to.eq(404);
+      expect(res.body).to.have.property("routes");
+    });
+  });
+});
---
+++ cypress.config.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { defineConfig } from "cypress";
+import db from "backend/db/db";
+
+export default defineConfig({
+  env: {
+    apiUrl: process.env.BASE_URL + "/api",
+  },
+  component: {
+    specPattern: ["**/*.cy.{js,jsx,ts,tsx}", "**/*.test.{js,jsx,ts,tsx}"],
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    setupNodeEvents(on, _) {
+      on("task", {
+        async initDB() {
+          await db.init();
+          return null;
+        },
+      });
+    },
+  },
+});
