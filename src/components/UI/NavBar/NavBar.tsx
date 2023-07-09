b3e2b2a78e4ef360175bd9ccb2970f6bea2cf47e
Hi Tran <thuchitran@gmail.com>
2023-07-09 23:54:29 +1000

    style responsive navbar

--- src/components/UI/NavBar/NavBar.tsx
+++ src/components/UI/NavBar/NavBar.tsx
@@ -1,7 +1,112 @@
+import React, { useState, useEffect } from "react";
 import styles from "./NavBar.module.css";
+import user_profile from "../../../assets/user_profile.png";
+import Icon from "../Icon/Icon";
 
-const NavBar = (props) => {
-  return <div className={styles.navbar}>Nav Bar</div>;
+type NavBarProps = {
+  active: string;
+};
+
+const NavBar: React.FC<NavBarProps> = (props) => {
+  const [isResized, setIsResized] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMenuShown, setIsMenuShown] = useState(false);
+
+  useEffect(() => {
+    if (!isResized) {
+      screenSizeHandler();
+    }
+    checkScreenResize();
+  }, [isResized]);
+
+  const checkScreenResize = () => {
+    window.addEventListener("resize", () => {
+      if (!isResized) {
+        setIsResized(true);
+      }
+      screenSizeHandler();
+    });
+  };
+
+  const screenSizeHandler = () => {
+    if (window.innerWidth > 760) {
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+    }
+  };
+
+  const toggleMenuHandler = () => {
+    setIsMenuShown((currentMenuState) => !currentMenuState);
+  };
+
+  const userProfile = (
+    <div className={styles["navbar__user-profile"]}>
+      {/* TO DO Update img src with real user profile pic */}
+      <img src={user_profile}></img>
+    </div>
+  );
+
+  const menu = (
+    <ul className={styles["navbar__menu"]}>
+      <li>
+        <span>
+          <Icon name="dashboard" />
+        </span>
+        Dashboard
+      </li>
+      <li>
+        <span>
+          <Icon name="cart" />
+        </span>
+        Purchase Ticket
+      </li>
+      <li>
+        <span>
+          <Icon name="customer" />
+        </span>
+        Customers
+      </li>
+      <li>
+        <span>
+          <Icon name="ticket" />
+        </span>
+        Tickets
+      </li>
+      <li>
+        <span>
+          <Icon name="clock" />
+        </span>
+        Bookings
+      </li>
+    </ul>
+  );
+
+  return (
+    <div className={styles.navbar}>
+      {isMobile && (
+        <>
+          <div className={styles["navbar__mobile-header"]}>
+            {userProfile}
+            <div
+              className={styles["navBar__menu-icon"]}
+              onClick={toggleMenuHandler}
+            >
+              <Icon name={isMenuShown ? "close" : "hamburger"} />
+            </div>
+          </div>
+          {isMenuShown && menu}
+        </>
+      )}
+
+      {!isMobile && (
+        <>
+          <div>{userProfile} </div>
+          {menu}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default NavBar;
