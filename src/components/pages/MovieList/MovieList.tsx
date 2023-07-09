bd7b929b22ccae2cb545c841deaa51c9f3d0b9d8
Hi Tran <thuchitran@gmail.com>
2023-07-09 15:39:25 +1000

    add table component

--- src/components/pages/MovieList/MovieList.tsx
+++ src/components/pages/MovieList/MovieList.tsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from "react";
+import { fetchMovies } from "../../../utils/http-requests";
+import Table from "../../UI/Table/Table";
+
 const MovieList = () => {
-  return <div>Movie List</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, []);
+
+  const fetchMovieHandler = async () => {
+    const movieList = await fetchMovies();
+    if (movieList) {
+      setMovies(movieList);
+    }
+  };
+  return <Table type="movie" values={movies}></Table>;
 };
 
 export default MovieList;
