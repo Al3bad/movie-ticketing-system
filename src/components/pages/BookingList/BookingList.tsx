6d8a33e88fc6868418d368f6edb925a14827d666
Hi Tran <thuchitran@gmail.com>
2023-07-11 07:55:27 +1000

    add Table to bookingList

--- src/components/pages/BookingList/BookingList.tsx
+++ src/components/pages/BookingList/BookingList.tsx
@@ -1,5 +1,45 @@
+import { useState, useEffect } from "react";
+import { fetchBookings } from "../../../utils/http-requests";
+import Table from "../../UI/Table/Table";
+
+const table_headers = [
+  { title: "Name", key: "name" },
+  { title: "Movie", key: "movie" },
+  { title: "Email", key: "email" },
+  { title: "Ticket Qty", key: "qty" },
+];
+
 const BookingList = () => {
-  return <div>Bookings</div>;
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetchBookingHandler();
+  }, []);
+
+  const fetchBookingHandler = async () => {
+    const bookingList = await fetchBookings();
+    if (bookingList) {
+      const bookingValues = bookingList.map((booking: Booking) => {
+        return {
+          email: booking.customer.email,
+          name: booking.customer.name,
+          movie: booking.title,
+          qty: getTicketQty(booking),
+        };
+      });
+      setBookings(bookingValues);
+    }
+  };
+
+  const getTicketQty = (booking: Booking) => {
+    const ticketQties = booking.tickets.map((ticket) => ticket.qty);
+    const sumTicketQty = ticketQties.reduce(
+      (sum: number, qty: number) => sum + qty
+    );
+    return sumTicketQty;
+  };
+
+  return <Table headers={table_headers} values={bookings}></Table>;
 };
 
 export default BookingList;
