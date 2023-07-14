e50c40080a659c494016578ef1489cc15e2e2b49
Hi Tran <thuchitran@gmail.com>
2023-07-14 13:44:51 +1000

    add movie detail

--- src/components/pages/MovieDetail/MovieDetail.tsx
+++ src/components/pages/MovieDetail/MovieDetail.tsx
@@ -1,43 +1,74 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { fetchMovieByTitle } from "../../../utils/http-requests";
+import Input from "../../UI/Input/Input";
+import Button from "../../UI/Button/Button";
 
 const inputFields = [
   {
     title: "Title",
     key: "title",
+    type: "text",
   },
   {
     title: "Seat Available",
     key: "seatAvailable",
-  },
-  {
-    title: "Is Released",
-    key: "isReleased",
+    type: "number",
   },
 ];
 
 const MovieDetail = () => {
+  const [movie, setMovie] = useState();
+  const [isEdit, setIsEdit] = useState(false);
+  const params = useLocation();
+  useEffect(() => {
+    const query = new URLSearchParams(params.search);
+    const title = query.get("id");
+    if (title) {
+      fetchMovieHandler(title);
+    }
+  }, []);
+
+  const fetchMovieHandler = async (title: string) => {
+    const fetchedMovie = await fetchMovieByTitle(title);
+    if (fetchedMovie) {
+      setMovie(fetchedMovie);
+    }
+  };
+
+  const inputChangeHandler = (e) => {
+    console.log(e);
+  };
+
+  const editMovieHandler = () => {
+    // Toggle the current edit state
+    setIsEdit((currentState) => !currentState);
+  };
+
   return (
     <>
-      <form>
-        {inputFields.map((field, id) => {
-          return (
-            <Input
-              key={id}
-              isDisabled={field.key === "type" ? true : !isEdit}
-              label={field.label}
-              type={field.type}
-              onChange={inputChangeHandler}
-              value={ticket[field.key]}
-            />
-          );
-        })}
-        <Button
-          label="Edit"
-          type="button"
-          onClick={editCustomerHandler}
-          classLabels={["primary"]}
-        />
-      </form>
+      {movie && (
+        <form>
+          {inputFields.map((field, id) => {
+            return (
+              <Input
+                key={id}
+                isDisabled={!isEdit}
+                label={field.label}
+                type={field.type}
+                onChange={inputChangeHandler}
+                value={movie[field.key]}
+              />
+            );
+          })}
+          <Button
+            label={!isEdit ? "Edit" : "Save"}
+            type="button"
+            onClick={editMovieHandler}
+            classLabels={["primary"]}
+          />
+        </form>
+      )}
     </>
   );
 };
