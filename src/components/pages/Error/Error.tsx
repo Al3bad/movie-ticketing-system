eacc5ab80f89a6f5771178109f0ad1e059ab1e02
Hi Tran <thuchitran@gmail.com>
2023-07-16 12:20:21 +1000

    add generic error handler page

--- src/components/pages/Error/Error.tsx
+++ src/components/pages/Error/Error.tsx
@@ -1,13 +1,28 @@
+import { useRouteError } from "react-router-dom";
 import NavBar from "../../UI/NavBar/NavBar";
 import styles from "./Error.module.css";
 
 const Error = () => {
+  const error = useRouteError();
+  let message = "";
+  let status = "";
+  if (error) {
+    if (error.message) {
+      message = error.message;
+    } else {
+      message = error.statusText; // default error message of useRouterError
+    }
+
+    if (error.status) {
+      status = error.status;
+    }
+  }
   return (
     <div className={styles.container}>
       <NavBar className={styles.container__navbar} />
       <main className={styles.container__content}>
-        <h1>Page Not Found</h1>
-        <p>An Error Occurred!</p>
+        <h1>{status}</h1>
+        <h1>{message}</h1>
       </main>
     </div>
   );
